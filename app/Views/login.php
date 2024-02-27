11ad43818f9ba05ec4917e90b46e066eaf5cd59f
NyanPasu123 <jojogo2308@gmail.com>
2024-02-27 21:17:30 +0700

    first commit

--- app/Views/login.php
+++ app/Views/login.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Pages / Login to Galeri</title>
+  <title>Pages / Login to Childcare</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -51,7 +51,7 @@
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                   <img src="<?= base_url('assets/img/p.png')?>" alt="">
-                  <span class="d-none d-lg-block">Permata Harapan Galery</span>
+                  <span class="d-none d-lg-block">Permata Harapan Childcare</span>
                 </a>
               </div><!-- End Logo -->
 
@@ -61,7 +61,7 @@
 
                   <div class="pt-4 pb-2">
                     <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login the galery</p>
+                    <p class="text-center small">Enter your username & password to login the childcare</p>
                   </div>
 
                   <form class="row g-3 needs-validation" novalidate action="<?=base_url('home/aksi_login')?>"method="post">
